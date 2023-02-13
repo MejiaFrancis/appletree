b91128cf07d8cb3adfae760d3f948a545cbfa30c
Francis D. Mejia Jr <200116132@ub.edu.bz>
2023-02-13 14:42:47 -0600

    made changes to schools.go

--- cmd/api/schools.go
+++ cmd/api/schools.go
@@ -5,6 +5,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 // createSchoolHandler for the "POST /v1/schools" endpoint
@@ -14,8 +17,10 @@ func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Reque
 
 // showSchoolHandler for the "GET /v1/schools/:id" endpoint
 func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request) {
-	id, err := app.readIDParam(r)
-	if err != nil {
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	//id, err := app.readIDParam(r)
+	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}

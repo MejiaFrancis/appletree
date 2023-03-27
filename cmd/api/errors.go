285c88a8ee93bb1ae0d0e52e32efbd07b2e5a644
Francis D. Mejia Jr <200116132@ub.edu.bz>
2023-03-27 15:09:12 -0600

    added some error handlers

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -1,17 +1,17 @@
-
 package main
 
 import (
 	"fmt"
 	"net/http"
 )
+
 // log an error to console
 func (app *application) logError(r *http.Request, err error) {
 	app.logger.Println(err)
 }
 
 // respond in JSON
-func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{} ) {
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
 	env := envelope{"error": message}
 	err := app.WriteJSON(w, status, env, nil)
 	if err != nil {
@@ -24,17 +24,21 @@ func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, st
 func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.logError(r, err)
 	message := "the server encountered a problem and cannot process the request "
-	app.errorResponse(w,r, http.StatusInternalServerError, message)
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
 }
 
 // log an error to console
 func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
 	message := "the requested resource could not be found "
-	app.errorResponse(w,r, http.StatusNotFound, message)
+	app.errorResponse(w, r, http.StatusNotFound, message)
 }
 
 // method not allowed
 func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
-	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method) 
-	app.errorResponse(w,r, http.StatusMethodNotAllowed, message)
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -5,8 +5,11 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -42,5 +45,58 @@ func (app *application) WriteJSON(w http.ResponseWriter, status int, data envelo
 	w.WriteHeader(status)
 	// Write the []byte slice containing the JSON response body
 	w.Write(js)
+	return nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, desitination interface{}) error {
+	// specify the max size of our JSON request body
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	// we will try to decode the JSON request
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	// start the decoding
+	err := dec.Decode(desitination)
+	if err != nil {
+		//something went wrong
+		// let's find out the type of error
+		var syntaxError *json.SyntaxError
+		var unMarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+		// check for max bytes
+		var maxBytesError *http.MaxBytesError
+		// let's check for the type of decode error
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON(at character  %d)", syntaxError.Offset)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+		case errors.As(err, &unMarshalTypeError):
+			// whixk field has the wrong type
+			if unMarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unMarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains badly-formed JSON(at character %d)", unMarshalTypeError.Offset)
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+			// unmappable field/ not -ecistent field
+		case strings.HasPrefix(err.Error(), "json: unknown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+		case errors.As(err, &maxBytesError):
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+		default:
+			return err
+		}
+
+	}
+	// Let's call the decoder agian to check if ther are any
+	// trailing json objects
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
 	return nil
 }
